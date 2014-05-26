deb9dddd8f6b77440661207c6b7472f564e4f178
Brian Goff <cpuguy83@gmail.com>
2014-05-26 19:23:55 -0400

    Add password opt and default openvpn opts

--- start.sh
+++ start.sh
@@ -3,4 +3,25 @@
 [[ -d /dev/net ]] || mkdir -p /dev/net
 [[ -c /dev/net/tun ]] || mknod /dev/net/tun c 10 200
 
-exec /usr/local/openvpn_as/scripts/openvpnas -n
+if [ ! -f /tmp/pass_set ]; then
+  if [ ! -z ${OPENVPN_PASS} ]; then
+    echo "${OPENVPN_PASS}" | passwd --stdin openvpn
+    pass=${OPENVPN_PASS}
+  else
+    pass=$(openssl rand -base64 32)
+    echo "${pass}" | passwd --stdin openvpn
+  fi
+fi
+
+touch /tmp/pass_set
+
+echo "To login to OpenVPN, use the following credentials:"
+echo "username: openvpn"
+echo "password: ${pass}"
+
+if [ -f /vpn.pid ]; then
+  echo "Not shutdown properly, removing stale pid"
+  rm /vpn.pid
+fi
+
+exec /usr/local/openvpn_as/scripts/openvpnas -n --pidfile /vpn.pid -l -
